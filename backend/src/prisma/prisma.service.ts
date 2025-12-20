753dd6221c1fba3b9974121c9d326058af182ee3
Maxience <dossoumaxime888@gmail.com>
2025-12-20 12:09:16 +0100

    fix: force prisma connection string for vercel

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,8 +1,20 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { join } from 'path';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: process.env.VERCEL === '1'
+                        ? `file:${join(process.cwd(), 'prisma', 'dev.db')}`
+                        : process.env.DATABASE_URL
+                }
+            }
+        });
+    }
 
     async onModuleInit() {
         await this.$connect();
